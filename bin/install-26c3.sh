7bb1569c6c8fab328934264cb29b9735901a471e
Thomas Kollbach <thomas@kollba.ch>
2009-12-16 00:47:38 +0100

    fixed mongrel cluster config not pretty but works for now

--- bin/install-26c3.sh
+++ bin/install-26c3.sh
@@ -4,7 +4,7 @@ export UNSTABLE="deb http://http.us.debian.org/debian unstable main contrib non-
 echo $UNSTABLE | cat - >> /etc/apt/sources.list
 aptitude update 
 aptitude safe-upgrade -y
-aptitude install build-essential openssh-server git-core ruby1.8 sudo rsync rubygems1.8 mongrel sqlite3 rake  vim ssh xorg gdm nginx libsqlite33-ruby1.8 libopenssl-ruby1.8 ruby1.8-dev libnokogiri-ruby1.8 
+aptitude install build-essential openssh-server git-core ruby1.8 sudo rsync rubygems1.8 mongrel sqlite3 rake  vim ssh xorg gdm nginx libsqlite33-ruby1.8 libopenssl-ruby1.8 ruby1.8-dev libnokogiri-ruby1.8 avahi-daemon  mongrel-cluster
 # aptitude install --with-recommends bastille
 aptitude install --with-recommends epiphany-webkit arora
 
@@ -16,6 +16,7 @@ chmod -R g-w $DESTINATION/.ssh
 export SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAxG4eu4c7vt9nfzW475+clZseDQAZ8EOvPYP10XahkFlFxyxdw4rwKUqDk4v9YqsOEd71/9XdZbebVQwz4qRvN0VoWdtER9gHHEALGMzfEL7iavcqTECg4d9NKqNKyfu67RDCvdDdUrAoTFFi0UQe5LBa3NxL9jQDHz6HecdmN80= toto@amber.lan"
 echo $SSH_KEY | cat - > $DESTINATION/.ssh/autorized_keys2
 git clone git://github.com/toto/pentabarf-annoncer.git $DESTINATION/pentabarf-annoncer
+cd $DESTINATION/pentabarf-annoncer && git pull
 
 cp $DESTINATION/pentabarf-annoncer/config/database.yml{.example,}
 cd $DESTINATION/pentabarf-annoncer && rake gems:install
@@ -25,6 +26,14 @@ chown -R announcer $DESTINATION
 
 # create user
 useradd -m -s /bin/bash -p `mkpasswd www SD` www
+
+# enable services 
+update-rc.d avahi-daemon enable
+update-rc.d nginx enable
+
+mkdir -p /etc/mongrel_cluster/
+cp $DESTINATION/pentabarf-annoncer/config
+
 sleep 2		
 	
 	
