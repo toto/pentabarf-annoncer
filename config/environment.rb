1d07d7f8b9ca5d32b34878017a8e9e877d866908
Thomas Kollbach <thomas@kollba.ch>
2009-12-16 02:00:00 +0100

    UTC timezone

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = :utc # FIXME: Pentabarf makes not indication about timezone in the XML export…
+  config.time_zone = 'UTC' # FIXME: Pentabarf makes not indication about timezone in the XML export…
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
