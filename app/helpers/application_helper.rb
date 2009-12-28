2b49f1d00e479d3b5f871215b2b112c28f51aacd
Niklas Hofer <niklas+dev@lanpartei.de>
2009-12-28 04:21:53 +0100

    #jquery and # periodically_refresh_room helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,23 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def jquery(*args, &block)
+    html_options = args.extract_options!
+    content = returning '' do |js|
+      js << '(function($){' 
+      if block
+        js << capture(&block)
+      else
+        js << args.first
+      end
+      js << '})(jQuery);'
+    end
+    tag = content_tag(:script, javascript_cdata_section(content), html_options.merge(:type => Mime::JS, :defer => 'defer'))
+
+    if block_called_from_erb?(block)
+      concat(tag)
+    else
+      tag
+    end
+  end
+
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  def periodically_refresh_room(room, interval = Event::REFRESH_TIME )
+    js = <<-EOJS
+      var refresh_room = function() {
+        getEventsAndUpdateList("#{room.id}", "#{Event::MAX_OTHER_ROOM_EVENTS}" )
+      };
+      refresh_room();
+      setInterval ( refresh_room, #{interval.to_i});
+    EOJS
+    jquery js
+  end
 end
