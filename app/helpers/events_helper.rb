f7be5cd9de39beb7270ff836d56646690b36806f
Thomas Kollbach <thomas@kollba.ch>
2009-12-28 11:31:25 +0100

    MAX_THIS_ROOMS_EVTNS is respected now

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,8 @@
 module EventsHelper
-  def periodically_refresh_room(room, interval = Event::REFRESH_TIME )
+  def periodically_refresh_room(room, limit = Event::MAX_OTHER_ROOM_EVENTS, interval = Event::REFRESH_TIME )
     js = <<-EOJS
       var refresh_room = function() {
-        getEventsAndUpdateList("#{room.id}", "#{Event::MAX_OTHER_ROOM_EVENTS}" )
+        getEventsAndUpdateList("#{room.id}", "#{limit}" )
       };
       refresh_room();
       setInterval ( refresh_room, #{interval.to_i});
