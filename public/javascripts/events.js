7ae6bcc469291ca8e86d092ebf32604ba4639673
Niklas Hofer <niklas+dev@lanpartei.de>
2009-12-28 03:57:47 +0100

    made jslint happy

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,4 +1,8 @@
+/*jslint browser: true */
+/*global jQuery, Toolbox */
 // This was previously placed in app/views/events/index.html.erb
+
+(function($){
 
   function sortListElements(list_selector, comparison_function) {
     var mylist = $(list_selector);
@@ -21,19 +25,19 @@
   }
 
   function domIdForEvent(this_event) {
-    return 'room_' + this_event['room_id'] + '_event_' + this_event['id'];
+    return 'room_' + this_event.room_id + '_event_' + this_event.id;
   }
 
   function makeListEntry(this_event) {
-    var date =  Date.parse(this_event['start_time']);
+    var date =  Date.parse(this_event.start_time);
 
     var li = '<li id="' + domIdForEvent(this_event) +
     '" class="event" style="display: none">'+
-    '<div class="js_date">'+ this_event['js_date'] + "</div>" +
+    '<div class="js_date">'+ this_event.js_date + "</div>" +
     '<dl><dt class="title">' + 
-    '<span class="start_time' + (this_event['current'] ? ' current' : '' )  + '">' + this_event['human_start_time'] + '</span>' +
-    this_event['title'] + '</dt>' +
-    '<dt class="subtitle">' + this_event['subtitle'] + '</dt>' +
+    '<span class="start_time' + (this_event.current ? ' current' : '' )  + '">' + this_event.human_start_time + '</span>' +
+    this_event.title + '</dt>' +
+    '<dt class="subtitle">' + this_event.subtitle + '</dt>' +
     '<dd class="people">' + '' + '</dd>' +
     '</dl>' + "</li>";
     
@@ -57,20 +61,20 @@
     $.getJSON(url,
               function(data) {
                 jQuery.each(data, function(index) {
-                  var entry = makeListEntry(this['event']);
-                  if( $('#' + domIdForEvent(this['event'])).length ) {
-                    $('#' + domIdForEvent(this['event'])).replaceWith(entry);
-                    $('#' + domIdForEvent(this['event'])).show();
+                  var entry = makeListEntry(this.event);
+                  if( $('#' + domIdForEvent(this.event)).length ) {
+                    $('#' + domIdForEvent(this.event)).replaceWith(entry);
+                    $('#' + domIdForEvent(this.event)).show();
                   }
                   else {           
                     entry.appendTo("#room_" + room_id + "_events");
-                    $('#' + domIdForEvent(this['event'])).toggle("slow");                    
+                    $('#' + domIdForEvent(this.event)).toggle("slow");                    
                   }
 
 
                 });
                 var ids_from_server = jQuery.map(data, function(n,i) {
-                  return domIdForEvent(n['event']);
+                  return domIdForEvent(n.event);
                 });
                 var ids_on_page = jQuery.map($("#room_" + room_id + "_events li"), function(n,i) {
                   return n.id;
@@ -81,9 +85,10 @@
                   var inArray = false;
                   for (var i = ids_from_server.length - 1; i >= 0; i--){
                     inArray = (ids_from_server[i] == this);
-                    if(inArray)
+                    if(inArray) {
                       break;
-                  };
+                    }
+                  }
                   
                   console.log("in array: " + inArray);
                   
@@ -97,4 +102,4 @@
                 sortListElementsByStartDate("#room_" + room_id + "_events");
               });    
   }
-
+})(jQuery);
